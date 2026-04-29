9b1cfbf66873a7939d04eb1fc050b97b6100c436
gianAchei <gian@acheipneus.com.br>
2026-04-28 21:31:44 -0300

    feat: gian function

--- src/function_app.py
+++ src/function_app.py
@@ -18,4 +18,13 @@ def timer_trigger_2(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('TIMER_TRIGGER_2 running')
+    logging.info('TIMER_TRIGGER_2 running') 
+
+@app.timer_trigger(schedule="0 */6 * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger_gian(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed GIAN.')
